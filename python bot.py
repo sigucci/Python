7a0e6170e93e94f1b09093daebfe9c189607207b
Pavel <128320059+sigucci@users.noreply.github.com>
2023-04-05 11:32:35 +0300

    Update python bot.py

--- python bot.py
+++ python bot.py
@@ -6,29 +6,22 @@ import logging
 from aiogram import Bot, Dispatcher, executor, types
 from pythontest import SQLighter
 
-#Задаем уровень логов
 logging.basicConfig(level=logging.INFO)
 
-#Инициализация бота
 bot = Bot(token=pythonconfig.API_TOKEN)
 dp = Dispatcher(bot)
 
-#Инициализация соединения с БД
 db = SQLighter('db.db')
 
-#Команда активации подписки
 @dp.message_handler(commands=['subscribe'])
 async def subscribe(message: types.Message):
     if(not db.subscriber_exists(message.from_user.id)):
-        #Если юзера нет в базе, добавляем его
         db.add_subscriber(message.from_user.id)
     else:
-        #Если пользователь есть, обновляем статус подписки
         db.update_subscriptions(message.from_user.id, True)
 
     await message.answer("Вы успешно подписались на рассылку!")
     
-#Команда отписки
 @dp.message_handler(commands=['unsubscribe'])
 async def unsubscribe(message: types.Message):
     if(not db.subscriber_exists(message.from_user.id)):
